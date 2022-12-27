442a8c6cee0ab3845347c69c821679f374bb3109
jacob <jvnguyen@ualberta.ca>
2022-12-27 00:39:55 -0700

    defined draw information and creating a list

--- sortVisualization.py
+++ sortVisualization.py
@@ -16,3 +16,20 @@ class DrawInformation:
 
     SIDE_PAD = 100
     TOP_PAD = 150
+
+    def __init__(self, width, height, list):
+        self.width = width
+        self.height = height
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visualization")
+        self.set_list(list)
+
+    def set_list(self, list):
+        self.list = list
+        self.min_val = min(list)
+        self.max_val = max(list)
+
+        self.block_width = round((self.width - self.SIDE_PAD) / len(list))
+        self.block_height = math.floor((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
+
