d8c543eaf894d2c87ccecdebd01a4c4313c0ef3c
jacob <jvnguyen@ualberta.ca>
2022-12-28 01:20:38 -0700

    fixed visual bug where red was appearing on the screen

--- sortVisualization.py
+++ sortVisualization.py
@@ -43,6 +43,12 @@ def draw(draw_info, algorithm_name, ascending):
 
 def draw_list(draw_info, color_positions={}, clear_background=False):
     list = draw_info.list
+
+    if clear_background:
+        clear_rect = (draw_info.SIDE_PAD // 2, draw_info.TOP_PAD,
+                      draw_info.width - draw_info.SIDE_PAD, draw_info.height - draw_info.TOP_PAD)
+        pygame.draw.rect(draw_info.window, draw_info.BACKGROUND_COLOR, clear_rect)
+
     for i, val in enumerate(list):
         x = draw_info.start_x + i * draw_info.block_width
         y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
