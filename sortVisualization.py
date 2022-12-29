0dda0171028920c32b92b07473abbe6f354d5a02
jacob <jvnguyen@ualberta.ca>
2022-12-28 21:34:07 -0700

    added visual controls

--- sortVisualization.py
+++ sortVisualization.py
@@ -37,6 +37,18 @@ class DrawInformation:
 
 def draw(draw_info, algorithm_name, ascending):
     draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+
+    title = draw_info.LARGE_FONT.render(f"{algorithm_name} - {'Ascending' if ascending else 'Descending'}", 1,
+                                        draw_info.BLUE)
+    draw_info.window.blit(title, (draw_info.width / 2 - title.get_width() / 2, 5))
+
+    controls = draw_info.FONT.render("R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending", 1,
+                                     draw_info.BLACK)
+    draw_info.window.blit(controls, (draw_info.width / 2 - controls.get_width() / 2, 45))
+
+    sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.BLACK)
+    draw_info.window.blit(sorting, (draw_info.width / 2 - sorting.get_width() / 2, 75))
+
     draw_list(draw_info)
     pygame.display.update()
 
@@ -145,7 +157,7 @@ def main():
                 list = generate_list(n, minVAL, maxVal)
                 draw_info.set_list(list)
                 sorting = False
-            if event.key == pygame.K_SPACE and sorting == False:
+            elif event.key == pygame.K_SPACE and sorting == False:
                 sorting = True
                 sortingAlgorithmGenerator = sortingAlgorithm(draw_info, ascending)
             elif event.key == pygame.K_i and not sorting:
